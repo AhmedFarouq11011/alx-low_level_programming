bbe7adb8d5720633ca0c779bc67033a0dd842761
ahmedfarouq11011 <ahmedfarouq3.14@gmail.com>
2023-11-14 00:37:20 -0800

    adding 2-strlen for correction

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strlen_recursion  - prints the string followed by a new line
+ *@s :the string to be used 
+ *
+ * Return: Always 0.
+ */
+
+int _strlen_recursion(char *s);
+
+int i = 0;
+
+int _strlen_recursion(char *s)
+{
+	if (s[i] == '\0')
+	{
+		return i;
+	}
+	else
+	{
+		i++;
+		 return _strlen_recursion(s);
+	}
+
+}
+
