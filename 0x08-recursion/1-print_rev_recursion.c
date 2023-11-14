c1fa9b5e57a6e918129b6269680ce875e2298286
ahmedfarouq11011 <ahmedfarouq3.14@gmail.com>
2023-11-13 23:51:59 -0800

    adding 1-prin_rev_recursion.c for correction check

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ *_print_rev_recursion - prints the string followed by a new line
+ *@s :the string to be printed
+ *
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s);
+
+int i = 0;
+void _print_rev_recursion(char *s)
+{
+	int x = strlen(s);
+	if (s[x-i] == '\0')
+	{
+
+		_putchar(s[x-i-1]);
+		i += 2;
+		_print_rev_recursion(s);
+	}
+	else if (x-i == 0)
+	{
+		_putchar(s[x-i]);
+	}
+	else
+	{
+		_putchar(s[x-i]);
+		i++;
+		_print_rev_recursion(s);
+	}
+}
+
