2ec4970ce57527050ec60064168d98dd1a06a54f
ahmedfarouq11011 <ahmedfarouq3.14@gmail.com>
2023-11-14 00:44:43 -0800

    adding 2-strlen for style correction

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,7 +4,7 @@
 
 /**
  *_strlen_recursion  - prints the string followed by a new line
- *@s :the string to be used 
+ *@s :the string to be used
  *
  * Return: Always 0.
  */
@@ -17,12 +17,12 @@ int _strlen_recursion(char *s)
 {
 	if (s[i] == '\0')
 	{
-		return i;
+		return (i);
 	}
 	else
 	{
 		i++;
-		 return _strlen_recursion(s);
+		return (_strlen_recursion(s));
 	}
 
 }
