e1fce44cfdd08bee9b07c41812d7dda1e9404fba
ahmedfarouq11011 <ahmedfarouq3.14@gmail.com>
2023-11-14 01:21:45 -0800

    adding factorial for correctio

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ *factorial  - prints the factorial of a number
+ *@n :the int to be used
+ *
+ * Return: Always 0.
+ */
+
+int factorial(int n);
+
+int i = 0;
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		
+		return ( n * (factorial(n - 1)));
+	}
+
+}
+
