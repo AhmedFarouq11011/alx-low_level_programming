ee7bc1792923631ad57fcd93dc21152363ac073d
ahmedfarouq11011 <ahmedfarouq3.14@gmail.com>
2023-11-16 01:14:45 -0800

    adding 6-prime_number.c for correction and style

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- *_is_prime_number  - return 0 if not prime 1 if it is
+ *is_prime_number :return 0 if not prime 1 if it is
  *@n :the int to be used
  *
  * Return: Always 0.
@@ -28,9 +28,9 @@ int is_prime_number(int n)
 	{
 		return (1);
 	}
-	else if(x >= 1 && i == n)
+	else if (x >= 1 && i == n)
 	{
-		return(0);
+		return (0);
 	}
 	else
 	{
